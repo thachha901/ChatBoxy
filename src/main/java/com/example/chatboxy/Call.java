1c9866513a1ca1b54c0403a010b8dcf49b5b9397
thachha901 <124699333+thachha901@users.noreply.github.com>
2023-11-29 21:28:14 +0700

    up Call

---
+++ src/main/java/com/example/chatboxy/Call.java
@@ -0,0 +1,14 @@
+package com.example.chatboxy;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Call implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/com/example/chatboxy/Video.java
@@ -0,0 +1,4 @@
+package com.example.chatboxy;
+
+public class Video {
+}
